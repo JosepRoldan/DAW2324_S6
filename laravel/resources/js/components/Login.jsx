9dd9b26eff95c8f30c28b77c29e67c02a2711c69
illomedina <josemedina@iesmontsia.org>
2024-04-15 16:56:10 +0200

    Cambio login quitar cuadro de color

--- laravel/resources/js/components/Login.jsx
+++ laravel/resources/js/components/Login.jsx
@@ -52,13 +52,13 @@ export default function Login() {
     <>
         <Toaster richColors position="top-center"  />
             <section className="h-full bg-neutral-200 dark:bg-neutral-700">
-                <div className="container h-full p-10">
+                <div className="container h-full">
                     <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
                         <div className="w-full">
                             <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
                                 <div className="g-0 lg:flex lg:flex-wrap">
                                     <div className="px-4 md:px-0 lg:w-6/12">
-                                        <div className="md:mx-6 md:p-12">
+                                        <div className="ml-72 p-12 -mr-72">
                                             <div className="text-center">
                                                 <img
                                                     className="mx-auto w-48"
@@ -121,15 +121,6 @@ export default function Login() {
                                                 </div>
                                             </form>
                                         </div>
-                                    </div>
-
-                                    <div
-                                        className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
-                                        style={{
-                                            background:
-                                                "linear-gradient(to right, #512a8b, #7646b7, #9f6fc4, #c49ee3, #add8e6)",
-                                        }}
-                                    >
                                     </div>
                                 </div>
                             </div>
