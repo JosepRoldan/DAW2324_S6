9652d425103ebd5680e444af87530aca938e0de5
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-04-17 21:09:06 +0200

    added debugger for fastapi

--- fastapi/app/database.py
+++ fastapi/app/database.py
@@ -4,14 +4,14 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv("./app/.env")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:rootpwd@mysql:3306/customAIze"
 
 # Crear una instancia de motor
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
