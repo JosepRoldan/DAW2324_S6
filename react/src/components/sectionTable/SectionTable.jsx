80a9b67c34b9f1ef25aa3631c6869007a0be782c
l3lackJack <gabrielionuturs@iesebre.com>
2024-04-17 13:23:17 +0200

    Cerrando y abriendo el dropdown, añadiendo eventos en los formularios create y edit

--- react/src/components/sectionTable/SectionTable.jsx
+++ react/src/components/sectionTable/SectionTable.jsx
@@ -257,14 +257,16 @@ function SectionTable({ SectionName }) {
   };
 
   return (
-    <div className="flex flex-col h-[100vh] divContainer">
+    <div 
+    onClick={() => toggleDropdown()}
+    className="flex flex-col h-[100vh] divContainer">
       {loading && (
         <div className="loader-container">
           <div className="loader"></div>
         </div>
       )}
       <div className="relative group mb-10">
-        <button id="dropdown-button" onClick={toggleDropdown} className="inline-flex justify-center w-50 px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
+        <button id="dropdown-button" onClick={toggleDropdown} className="inline-flex justify-center w-50 px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm">
           <span className="mr-2">Select Year</span>
           <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fillRule="evenodd" d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
