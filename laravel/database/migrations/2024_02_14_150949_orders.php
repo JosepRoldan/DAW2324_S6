a85b3c3ebe1618fddd2c3192b99c7129c6803259
Miquel <miquelduran@iesmontsia.org>
2024-04-22 16:31:56 +0200

    migracions noves

---
+++ laravel/database/migrations/2024_02_14_150949_orders.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('number_order');
+            $table->foreignId('idCustomers')->references('id')->on('customers');
+            $table->string('name', 100);
+            $table->string('address', 100);
+            $table->decimal('totalPrice');
+            $table->timestamp('datetime');
+            $table->enum('orderStatus', ['PreOrder', 'InProgress', 'Sent', 'Delivered', 'Canceled'])->default('PreOrder');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
