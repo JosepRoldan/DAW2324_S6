acd808f1a14602850ec404edfb54eaa129cf1b2f
Macarena Gonzales <144931898+macarenagonzales@users.noreply.github.com>
2024-04-22 15:14:18 +0200

    actualizacion de modals para edicion y eliminacion

--- react/src/pages/users/UsersEdit.jsx
+++ react/src/pages/users/UsersEdit.jsx
@@ -11,13 +11,12 @@ export const UsersEdit = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const { state } = useLocation();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => setIsModalOpen(true);
-  const hideModal = () => setIsModalOpen(false);
+  //const showModal = () => setIsModalOpen(true);
+  //const hideModal = () => setIsModalOpen(false);
   const { setPage, setSteps } = usePage();
 
-  
-
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
   const users = state?.users;
 
   const userId = users.id;
@@ -67,14 +66,34 @@ export const UsersEdit = () => {
         errors.email = 'Formato de correo electrónico no válido.';
       }
     }
-    if (!formData.password) {
+    /*if (!formData.password) {
       errors.password = 'La contraseña es obligatoria';
     } else if (formData.password.length < 6) {
       errors.password = 'La contraseña debe tener al menos 6 caracteres.';
     }
-  
+  */
     return errors;
   };
+
+  const showModal = (modalType) => {
+    if (modalType === 'delete') {
+      setIsDeleteModalOpen(true);
+    } else if (modalType === 'update') {
+      setIsUpdateModalOpen(true);
+    }
+  };
+
+  const hideModal = (modalType) => {
+    if (modalType === 'delete') {
+      setIsDeleteModalOpen(false);
+    } else if (modalType === 'update') {
+      setIsUpdateModalOpen(false);
+    }
+  };
+
+
+
+
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -86,8 +105,8 @@ export const UsersEdit = () => {
 
   }
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmit = async (userId) => {
+    //e.preventDefault();
 
     const fieldErrors = validateForm();
 
@@ -119,8 +138,7 @@ export const UsersEdit = () => {
         body: JSON.stringify(formData),
       });
 
-      if (response.ok) {
-        alert('User updated successfully!');
+      if (response.ok) { 
         navigate('/users');
       } else {
         // Manejo de errores en caso de que la respuesta no sea exitosa
@@ -131,6 +149,7 @@ export const UsersEdit = () => {
       console.error('Error:', error);
     }
   }
+
 
   const onDelete = async (userId) => {
 
@@ -145,14 +164,12 @@ export const UsersEdit = () => {
       });
 
       if (response.ok) {
-        alert('User deleted successfully!');
         navigate('/users');
       } else {
-        alert('There was an error deleting the user');
+        console.error('There was an error deleting the user:', error);
       }
     } catch (error) {
       console.error('There was an error deleting the user:', error);
-      alert('There was an error deleting the user');
     }
   };
 
@@ -164,7 +181,7 @@ export const UsersEdit = () => {
   return (
 
     <>
-      {isModalOpen && (
+      {isDeleteModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
           <div className="bg-white p-4 sm:p-6 lg:p-8 shadow-xl rounded-lg">
             <h3 className="text-lg font-medium leading-6 text-gray-900">
@@ -179,8 +196,7 @@ export const UsersEdit = () => {
               <button
                 type="button"
                 className="mr-2 inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
-                onClick={hideModal}
-              >
+                onClick={() => hideModal('delete')}              >
                 {t("Cancel")}
               </button>
               <button
@@ -189,6 +205,36 @@ export const UsersEdit = () => {
                 onClick={() => { onDelete(userId); }}
               >
                 {t("Delete")}
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {isUpdateModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+          <div className="bg-white p-4 sm:p-6 lg:p-8 shadow-xl rounded-lg">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+            {t("Confirm Update")}
+            </h3>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">
+              {t("Are you sure you want to update this user?")} 
+              </p>
+            </div>
+            <div className="mt-4 flex justify-end">
+              <button
+                type="button"
+                className="mr-2 inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
+                onClick={() => hideModal('update')}              >
+                {t("Cancel")}
+              </button>
+              <button
+                type="button"
+                className="inAppLayout Page={'Edit User'} Steps={steps}line-flex justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                onClick={() => { onSubmit(userId); }}
+              >
+                {t("Update")}
               </button>
             </div>
           </div>
@@ -444,7 +490,7 @@ export const UsersEdit = () => {
 
           <div className="px-4 py-4 sm:px-6 flex justify-between items-center">
             {/* Botón a la izquierda */}
-            <button type="button" onClick={showModal}
+            <button type="button" onClick={() => showModal('delete')}
               className="inline-flex justify-center rounded-md bg-red-600 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -459,11 +505,12 @@ export const UsersEdit = () => {
                 className="inline-flex justify-center rounded-md bg-indigo-400 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
                 {t("Cancel")}
               </button>
-
-              <button type="submit" onClick={onSubmit}
+    
+              <button type="button" onClick={() => showModal('update')}
                 className="inline-flex justify-center rounded-md ml-2 bg-teal-400 px-3 py-2 text-md font-semibold text-blue-900 shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
                 {t("Update")}
               </button>
+              
             </div>
           </div>
 
