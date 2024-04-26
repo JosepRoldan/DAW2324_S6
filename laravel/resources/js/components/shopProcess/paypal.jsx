bee286cc340d88df8bd320b61341f15b8fd0e767
michipce <michipce@gmail.com>
2024-04-26 19:26:32 +0200

    muntat altra vegada

--- laravel/resources/js/components/shopProcess/paypal.jsx
+++ laravel/resources/js/components/shopProcess/paypal.jsx
@@ -1,3 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+function PayPalCheckout() {
+    function createOrder() {
+        return fetch("/my-server/create-paypal-order", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            // use the "body" param to optionally pass additional order information
+            // like product ids and quantities
+            body: JSON.stringify({
+                purchase_units: [
+                    {
+                        amount: {
+                            value: totalAmount.toFixed(2), // Importe total a cobrar por PayPal
+                            currency_code: "EUR", // Código de la moneda (puede variar según tu configuración)
+                        },
+                    },
+                ],
+            }),
+        })
+            .then((response) => response.json())
+            .then((order) => order.id);
+    }
+    function onApprove(data) {
+        return fetch("/my-server/capture-paypal-order", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                orderID: data.orderID,
+            }),
+        })
+            .then((response) => response.json())
+            .then((orderData) => {
+                const name = orderData.payer.name.given_name;
+                alert(`Transaction completed by ${name}`);
+            });
+    }
+    return (
+        <PayPalScriptProvider
+            options={{
+                clientId:
+                    "AUZhCdtjShVz3aPj-29oznCc8DHaY-fRbD9_83qAsarySyCRtDA41lKfkHC-PWglj8mC4YospxgDWzTX",
+            }}
+        >
+            <PayPalButtons />
+        </PayPalScriptProvider>
+    );
+}
+
+const rootElement = document.getElementById("paypal");
+if (rootElement) {
+    createRoot(rootElement).render(<PayPalCheckout />);
+}
